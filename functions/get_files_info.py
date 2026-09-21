4cec0fa2aa9ab45a073d831a36a2e28066a97ef5
localman-git <efitton5491@gmail.com>
2026-09-21 20:49:41 +0100

    starting tool build

---
+++ functions/get_files_info.py
@@ -0,0 +1,16 @@
+import os
+
+
+def get_files_info(working_directory: str, directory: str = ".") -> str:
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        valid_target_dir = os.path.commonpath([target_dir, working_dir_abs]) == working_dir_abs
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory.'
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory.'
+        else:
+            return f'Success: "{directory}" is within the working directory.'
+    except (ValueError, TypeError, OSError) as e:
+        return f"Error: {e}"
